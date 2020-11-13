baa23f8ac1373772a3173b8ae9a3f0ce756e2194
DEV <dev@info.local>
2020-11-13 09:40:14 -0300

    calculando idade

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace calculando_idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ano;
+            int idade;
+            Console.WriteLine("Em que ano você nasceu?");
+            ano = int.Parse(Console.ReadLine());
+            idade = 2020 - ano;
+            Console.Write("Você tem "+idade+" anos");
+        }
+    }
+}
